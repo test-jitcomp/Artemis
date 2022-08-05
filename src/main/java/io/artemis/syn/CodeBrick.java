b7e58551c874f3ea5d9190eeefe5b7852d214f12
Cong Li <cong.li@inf.ethz.ch>
2022-08-05 13:53:47 +0200

    Directly refactor the brick

--- src/main/java/io/artemis/syn/CodeBrick.java
+++ src/main/java/io/artemis/syn/CodeBrick.java
@@ -9,38 +9,46 @@ import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
 import spoon.support.util.ModelList;
 
+/**
+ * A code brick is a special type of code skeleton which have inputs and a list of statements. To
+ * use a code brick, synthesize a declaration for each input and link the statement. Sometimes, you
+ * may need to import the imports (but this often does not need since Spoon can take care of auto
+ * imports in its setting by Spoon.getEnvironment().setAutoImports(true)).
+ */
 /* package */ class CodeBrick {
-    /* package */ static final String CODE_BRICK_METHOD_NAME = "method";
-
     // The method that hangs the code brick
     private final CtMethod<?> mMethod;
+    // Imports that this code brick should import
     private final List<CtImport> mImports;
 
-    public CtParameter<?>[] getInputs() {
+    /**
+     * Get the inputs of this code brick. Just take care. The inputs returned are already linked. So
+     * please be sure to clone if they are expected to used elsewhere.
+     * 
+     * @return The inputs of this code brick.
+     */
+    public CtParameter<?>[] unsafeGetInputs() {
         List<CtParameter<?>> params = mMethod.getParameters();
         CtParameter<?>[] inputs = new CtParameter<?>[params.size()];
         for (int i = 0; i < inputs.length; i++) {
-            inputs[i] = params.get(i).clone();
+            inputs[i] = params.get(i);
         }
         return inputs;
     }
 
-    public CtBlock<?> getStatements() {
-        return mMethod.getBody().clone();
-    }
-
-    public List<String> getImports() {
-        return mImports.stream().map(i -> i.getReference().getSimpleName())
-                .collect(Collectors.toList());
+    /**
+     * Get all statements of this code brick. Just take care. The statements returned are already
+     * linked. So please be sure to clone if they are expected to used elsewhere.
+     * 
+     * @return All statements of this code brick.
+     */
+    public CtBlock<?> unsafeGetStatements() {
+        return mMethod.getBody();
     }
 
     @Override
     public String toString() {
         return mMethod.toString();
-    }
-
-    /* package */ List<CtParameter<?>> unsafeGetInput() {
-        return mMethod.getParameters();
     }
 
     /* package */ CodeBrick(CtMethod<?> cbMethod, List<CtImport> cbImports) {
--- src/main/java/io/artemis/syn/LoopSyn.java
+++ src/main/java/io/artemis/syn/LoopSyn.java
@@ -14,18 +14,11 @@ import io.artemis.AxRandom;
 import io.artemis.skl.ExHandleSkl;
 import io.artemis.skl.ForLoopSkl;
 import io.artemis.skl.RedirectSkl;
-import io.artemis.util.Pair;
 import io.artemis.util.Spoons;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtExpression;
-import spoon.reflect.code.CtLiteral;
 import spoon.reflect.code.CtLocalVariable;
-import spoon.reflect.code.CtNewArray;
 import spoon.reflect.code.CtStatement;
-import spoon.reflect.code.CtVariableAccess;
-import spoon.reflect.code.CtVariableRead;
-import spoon.reflect.code.CtVariableWrite;
-import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
@@ -33,10 +26,14 @@ import spoon.reflect.declaration.CtVariable;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 import spoon.reflect.reference.CtTypeReference;
-import spoon.reflect.reference.CtVariableReference;
-import spoon.reflect.visitor.CtAbstractVisitor;
-import spoon.reflect.visitor.CtScanner;
 
+/**
+ * A synthesizer which aims to synthesize a (neutral) loop at a given program point. Currently, the
+ * synthesizer prefers to synthesize a loop by using many program skeletons (the so-called code
+ * brick: class CodeBrick), synthesizing a declaration for each input of the brick, and finally
+ * connecting the brick with a loop header. To ensure neutral, it captures all potential exceptions
+ * likely to be thrown by the brick and redirect the output (stdout, stderr) to /dev/null.
+ */
 public class LoopSyn {
 
     private final Artemis mAx;
@@ -105,7 +102,6 @@ public class LoopSyn {
 
     private CtBlock<?> synLoopOnly(PPoint pp, Set<CtVariable<?>> reusedSet) {
         CtBlock<?> loop = mAx.getSpoon().getFactory().createBlock();
-        CtMethod<?> meth = pp.getMethod();
 
         // Choose a random code brick to instantiate
         CodeBrick cb = mCbManager.getCodeBrick(mRand.nextInt(mCbManager.getCbCount()));
@@ -113,15 +109,36 @@ public class LoopSyn {
             out.println(cb);
         });
 
-        // Find reusable variables and save which inputs should reuse which variable in a map
-        CtParameter<?>[] inputs = cb.getInputs();
-        List<Pair<CtParameter<?>, CtVariable<?>>> reusedMap = new ArrayList<>(inputs.length);
-        for (int i = 0; i < inputs.length; i++) {
-            CtParameter<?> inp = inputs[i];
-            CtTypeReference<?> inpType = inp.getType();
+        // Create declarations for reset inputs that are not to be replaced
+        synDecls(pp, cb.unsafeGetInputs(), reusedSet).forEach(loop::addStatement);
+
+        // Append the loop with the code brick as body
+        loop.addStatement(ForLoopSkl.instantiate(mAx,
+                /* iVarName= */ AxNames.getInstance().nextName(),
+                /* start= */ -mRand.nextInt(mAx.getMinLoopTrips()), /* step= */ mRand.nextInt(1, 2),
+                /* trip= */ mRand.nextInt(mAx.getMinLoopTrips(), mAx.getMaxLoopTrips()),
+                /* body= */ cb.unsafeGetStatements().clone()));
+
+        return loop;
+    }
+
+    private List<CtStatement> synDecls(PPoint pp, CtParameter<?>[] inputs,
+            Set<CtVariable<?>> reusedSet) {
+        Factory fact = mAx.getSpoon().getFactory();
+        CtMethod<?> meth = pp.getMethod();
+
+        List<CtStatement> decls = new ArrayList<>();
+        for (CtVariable<?> inp : inputs) {
+            CtTypeReference<?> inpType = inp.getType().clone();
+            String inpName = inp.getSimpleName();
+
+            // Let's find a reusable variables and replace all occurrences with that variable.
+            // We only consider reuse primitive types since reusing references (incl. array) is
+            // risky to be implicitly modified by our code brick. Just be careful.
+            // We always prefer to reuse existing variables than synthesize a new declaration.
             if (Spoons.isPrimitiveType(inpType)) {
                 List<CtVariable<?>> reusableSet = new ArrayList<>();
-                pp.forEachAccVariable(inp.getType(), var -> {
+                pp.forEachAccVariable(inpType, var -> {
                     // Ensure the same final; cannot use non-static in static environments
                     if (var.isFinal() == inp.isFinal()
                             && (!(var instanceof CtField) || var.isStatic() || !meth.isStatic())) {
@@ -129,75 +146,20 @@ public class LoopSyn {
                     }
                 });
                 if (reusableSet.size() > 0) {
+                    // Randomly select a variable, and rename all input occurrences
                     CtVariable<?> reusedVar =
                             reusableSet.get(AxRandom.getInstance().nextInt(reusableSet.size()));
-                    reusedMap.add(new Pair<>(inp, reusedVar));
+                    Spoons.renameVariable(inp, reusedVar.getSimpleName());
                     reusedSet.add(reusedVar);
-                    inputs[i] = null;
+                    continue;
                 }
             }
-        }
 
-        // Create declarations for reset inputs that are not to be replaced
-        synDecls(inputs).forEach(loop::addStatement);
-
-        // Append the loop with the code brick as body
-        loop.addStatement(ForLoopSkl.instantiate(mAx,
-                /* iVarName= */ AxNames.getInstance().nextName(),
-                /* start= */ -mRand.nextInt(mAx.getMinLoopTrips()), /* step= */ mRand.nextInt(1, 2),
-                /* trip= */ mRand.nextInt(mAx.getMinLoopTrips(), mAx.getMaxLoopTrips()),
-                /* body= */ cb.getStatements()));
-
-        // Rename variables in the loop by the reused variable. Since the loop hasn't been added
-        // to any tree, we cannot use any reliable way (like CtRenameGenericVariableRefactoring) to
-        // refactor. Here, let's apply some heuristic rules.
-        loop.accept(new CtScanner() {
-            @Override
-            protected void enter(CtElement e) {
-                e.accept(new CtAbstractVisitor() {
-                    @Override
-                    public <T> void visitCtVariableRead(CtVariableRead<T> vread) {
-                        perhapsReplace(vread);
-                    }
-
-                    @Override
-                    public <T> void visitCtVariableWrite(CtVariableWrite<T> vwrite) {
-                        perhapsReplace(vwrite);
-                    }
-
-                    private void perhapsReplace(CtVariableAccess<?> vacc) {
-                        CtVariableReference<?> var = vacc.getVariable();
-                        for (Pair<CtParameter<?>, CtVariable<?>> pair : reusedMap) {
-                            // Since the loop is not added, variable read/writes whose declarations
-                            // cannot be found are very likely to be our input
-                            if (pair.a.getSimpleName().equals(var.getSimpleName())
-                                    && var.getDeclaration() == null) {
-                                vacc.setVariable((CtVariableReference) pair.b.getReference());
-                            }
-                        }
-                    }
-                });
-            }
-        });
-
-        return loop;
-    }
-
-    private List<CtStatement> synDecls(CtVariable<?>[] inputs) {
-        Factory fact = mAx.getSpoon().getFactory();
-        List<CtStatement> decls = new ArrayList<>();
-        for (CtVariable<?> inp : inputs) {
-            // Null means this input has been synthesized
-            if (inp == null) {
-                continue;
-            }
-
-            CtTypeReference<?> inpType = inp.getType();
-            String inpName = inp.getSimpleName();
+            // If there's no reusable variables, let's try to find an initializer
             // TODO Call CbManager to reuse initializers (split initializers to many smaller ones)
-            CtExpression<?> inpInit = synExpr(inp.getType());
 
-            // TODO Add imports of the used initializer
+            // There's no initializers, either. Compromise. Let's do decl synthesis.
+            CtExpression<?> inpInit = synExpr(inpType);
 
             decls.add(fact.createLocalVariable(inpType, inpName, (CtExpression) inpInit));
         }
